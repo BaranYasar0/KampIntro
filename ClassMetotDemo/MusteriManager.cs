1fedcbefbceedeb1c1725b5ed5eb2096d0816159
baran <byasarn@gmail.com>
2022-10-04 16:53:21 +0300

    hafta 3 ödev

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+   public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Eklendi"+musteri.Isim);
+        }
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Silindi"+musteri.Isim);
+        }
+        public void MusteriListele(Musteri musteri)
+        {
+            Console.WriteLine("Lİst methodu cagırıldı."+musteri.Isim);
+        }
+    }
+
+
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            Musteri musteri2=new Musteri();
+            MusteriManager musteriManager=new MusteriManager();
+
+            musteri1.Isim = "omer";
+            musteri1.Id = "31";
+            musteri1.TcNo = "62";
+
+            musteri2.Isim = "baran";
+            musteri2.Id = "93";
+            musteri2.TcNo = "101";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            foreach (var temp in musteriler)
+            {
+                musteriManager.MusteriListele(temp);    
+            }
+            musteriManager.MusteriSil(musteri1);
+        }
+
+        
+    
+    }
+}
