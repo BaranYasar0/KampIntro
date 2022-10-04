6a6a1b1b15bcf0ab93cf0f11e3d7f670be384dc1
baran <byasarn@gmail.com>
2022-10-04 20:52:48 +0300

    Kamp 4 ödev

---
+++ GenericsHomework/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        MyDictionary<string> isimler1=new MyDictionary<string>();
+            Console.WriteLine(isimler1.Count);
+            isimler1.Add("Deneme");
+            Console.WriteLine(isimler1.Count);
+
+            isimler1.Add("yeter");
+            Console.WriteLine(isimler1.Count);
+
+            foreach (var temp in isimler1.Items)
+            {
+                Console.WriteLine(temp);
+            }
+        
+        
+        }
+    }
+}
