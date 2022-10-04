048d9127f135e009c7798767565b962aaa050f32
baran <byasarn@gmail.com>
2022-10-04 18:41:50 +0300

    kamp 4 ders sonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koleksiyonlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler2 = new List<string>(); 
+        }
+    }
+}
