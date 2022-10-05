6ba52439c335659e664f55c173e177f32abc2202
baran <byasarn@gmail.com>
2022-10-05 13:44:21 +0300

    Kamp 5 sonu

---
+++ OOP2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+GercekMusteri musteri1=new GercekMusteri();
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "baran";
+            musteri1.Soyadi = "yasar";
+            musteri1.Id = 1;
+
+            TuzelMusteri musteri2=new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo="123456";
+
+
+            Musteri musteri3=new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+            
+            CustomerManager customerManager1=new CustomerManager();
+
+        }
+
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        IKrediManager ihtiyackrediManager = new IhtiyacKrediManager();
+            TasitKrediManager tasitKrediManager = new TasitKrediManager();
+
+
+            ILoggerService databaseloggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService=new FileLoggerService();
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(tasitKrediManager,databaseloggerService);
+            basvuruManager.BasvuruYap(ihtiyackrediManager, fileLoggerService);
+
+            List<IKrediManager>krediler=new List<IKrediManager>() { ihtiyackrediManager,tasitKrediManager};
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
